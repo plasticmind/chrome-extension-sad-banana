4d54db61cb1133f1c608ab6543f6e1bd56111298
Jesse Gardner <jesse@plasticmind.com>
2019-09-16 10:47:56 -0400

    first (and likely last) commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+InboxSDK.load(2, 'sdk_gmail-apology_0976f5aa46').then(function(sdk){
+  /* 
+  * For people who put off responding for too long... 
+  * https://twitter.com/marcjohns/status/1173434159793967104
+  */
+
+  // the SDK has been loaded, now do something with it!
+  sdk.Compose.registerComposeViewHandler(function(composeView){
+    composeView.insertTextIntoBodyAtCursor('Sorry for not getting back to you sooner.');
+  });
+
+});
